fe234ad039d164fa86de17f950c93a272d3a6fee
slav-uk <contact.slav.uk@gmail.com>
2024-06-27 08:37:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function setLanguage(language) {
     document.documentElement.lang = language;
+    updateContent();
 }
+
+function updateContent() {
+    const allLangElements = document.querySelectorAll('[data-lang]');
+    allLangElements.forEach(element => {
+        element.style.display = 'none';
+    });
+
+    const activeLang = document.documentElement.lang;
+    const activeLangElements = document.querySelectorAll(`[data-lang="${activeLang}"]`);
+    activeLangElements.forEach(element => {
+        element.style.display = 'block';
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    updateContent();
+});
